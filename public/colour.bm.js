f33377b6224fbd6681d70272fc032710eae98071
Benjamin Reid <benjamin@nouveller.com>
2012-02-19 13:52:22 +0000

    Sandbox my version of jQuery

--- public/colour.bm.js
+++ public/colour.bm.js
@@ -5,48 +5,41 @@
       modal,
       iframeSrc     = 'http://takenby.rusic.com/colours/ideas/new',
       iframeWidth   = 750,
-      iframeHeight  = 260;
+      iframeHeight  = 260,
+      jq;
   
   // include jquery if it's not already on the page, otherwise run
-  if (typeof jQuery == 'undefined')
-  {
-    var s = document.createElement('script');
-    s.setAttribute('src','//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
-    s.onload = runColour;
-    document.body.appendChild(s);  
-  }
-  else
-  {
-    runColour();
-  }
+  var s = document.createElement('script');
+  s.setAttribute('src','//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
+  s.onload = runColour;
+  document.body.appendChild(s);  
 
   function runColour()
   {
-    (function($) {
+    jq = jQuery.noConflict(true);
 
-      // loop through all dom elements, exlcuding a couple
-      $('body *:not(script,style,noscript), body').each(function(i,el) {
-        
-        // get the element as a jquery object, and grab the two colour properties
-        var el        = $(el)
-            colour    = removeSpaces(el.css('color')),
-            bgColour  = removeSpaces(el.css('background-color'));
-        
-        if (!haveColour(colour) && colour != 'rgba(0,0,0,0)')
-        {
-          colours.push(colour);
-        }
-        if (!haveColour(bgColour) && bgColour != 'rgba(0,0,0,0)')
-        {
-          colours.push(bgColour);
-        }
-                   
-      });
+    // loop through all dom elements, exlcuding a couple
+    jq('body *:not(script,style,noscript), body').each(function(i,el) {
+      
+      // get the element as a jquery object, and grab the two colour properties
+      var el        = jq(el)
+          colour    = removeSpaces(el.css('color')),
+          bgColour  = removeSpaces(el.css('background-color'));
+      
+      if (!haveColour(colour) && colour != 'rgba(0,0,0,0)')
+      {
+        colours.push(colour);
+      }
+      if (!haveColour(bgColour) && bgColour != 'rgba(0,0,0,0)')
+      {
+        colours.push(bgColour);
+      }
+                 
+    });
 
-      var sendColours = removeBraces(removeQuotes(JSON.stringify(colours)));
+    var sendColours = removeBraces(removeQuotes(JSON.stringify(colours)));
 
-      loadRusicIframe(sendColours);         
-    })(jQuery);  
+    loadRusicIframe(sendColours);         
   }
 
   function haveColour(newColour)
@@ -65,6 +58,9 @@
   function loadRusicIframe(coloursToSend)
   {
     updateWindow();
+
+    var jFrame = jq('<iframe/>');
+    console.log(jFrame);
 
     // Styles for the iframe.
     var iframeStyle = 'position: fixed; z-index: 999999; width: '+iframeWidth+'px; height: '+iframeHeight+'px; left: '+((windowWidth/2)-(iframeWidth/2))+'px; top: '+((windowHeight/2)-(iframeHeight/2))+'px; border: none; overflow: hidden;';
@@ -87,17 +83,17 @@
 
   function updateWindow()
   {
-    windowWidth   = $(window).width(),
-    windowHeight  = $(window).height();
+    windowWidth   = jq(window).width(),
+    windowHeight  = jq(window).height();
   }
 
   function attachEvents()
   {
     // store a reference to the new injected modal
-    modal = $('#rusic-modal');
+    modal = jq('#rusic-modal');
 
     // update the window's width and height on resizing the browser
-    $(window).on('resize', function() {
+    jq(window).on('resize', function() {
       updateWindow();
       centreModal();  
     });
